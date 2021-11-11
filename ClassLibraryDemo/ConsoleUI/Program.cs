45b649192d2b15621c6ad2c4dc634fb34b2fb642
Sergii Cherevko <sergii.cherevko@thetradedesk.com>
2021-11-10 22:16:44 -0600

    First commit

---
+++ ClassLibraryDemo/ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Course:
+    //04 ОСНОВЫ C#: ОБЪЕКТНО-ОРИЕНТИРОВАННОЕ ПРОГРАММИРОВАНИЕ, ЧАСТЬ 1
+        //Video: 6 Class Libraries
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
