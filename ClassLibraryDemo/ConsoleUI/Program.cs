d90f8dbe59a68106294cf0aaedf56f7f83ccb7a6
Sergii Cherevko <sergii.cherevko@thetradedesk.com>
2021-11-10 23:21:47 -0600

    Add reference to the Library. Created instnace of a person model from the DemoLibrary in the ConsoleUI

--- ClassLibraryDemo/ConsoleUI/Program.cs
+++ ClassLibraryDemo/ConsoleUI/Program.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DemoLibrary.Models;
 //Course:
     //04 ОСНОВЫ C#: ОБЪЕКТНО-ОРИЕНТИРОВАННОЕ ПРОГРАММИРОВАНИЕ, ЧАСТЬ 1
         //Video: 6 Class Libraries
 
 namespace ConsoleUI
+// Note: In ConsoleUI you put only the code that importatant for the User Interface itself in the UI
+// Example: Console Rear/WriteLine
+// Everything else goes to the Class Library
+
+// Note: Two projects under the same Solution do not talk to each other. They can be connected through references
+// Steps: Right Click on References in UI > Add Reference > Projects > Choose your Library > Now have access to the Library and you can do: Using YouLibraryName.Models
+// Note2: You can NOT add reference to both projects, you'd get cercular dependancy error
 {
     class Program
     {
         static void Main(string[] args)
         {
+            PersonModel person = new PersonModel();
         }
     }
 }
--- ClassLibraryDemo/DemoLibrary/Calculation.cs
+++ ClassLibraryDemo/DemoLibrary/Calculation.cs
@@ -8,5 +8,10 @@ namespace DemoLibrary
 {
     public static class Calculation
     {
+        public static double Add(double x, double y) {
+            double output = 0;
+            output = x + y;
+            return output;
+        }
     }
 }
